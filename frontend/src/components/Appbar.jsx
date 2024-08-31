7f237be056ba33a6b006f3dee7f9d6cd1d4dddfc
aadil <aadikhanshaikh01@gmail.com>
2024-08-31 15:07:30 +0530

    addig lastName in appbar again

--- frontend/src/components/Appbar.jsx
+++ frontend/src/components/Appbar.jsx
@@ -30,12 +30,12 @@ export const AppBar = () => {
             </div>
             <div className="flex items-center">
                 <div className="flex flex-col justify-center h-full mr-4 text-sm font-medium">
-                    Hello, {username}
+                    Hello, {username} {lastName}
                 </div>
                 <div className="rounded-full h-12 w-12 bg-slate-200 flex justify-center items-center mt-1 mr-2">
                     <div className="text-xl font-semibold">
                         {username ? username.charAt(0).toUpperCase() : 'U'}
-                        {lastName ? lastName.charAt(0).toUpperCase() :""}
+                        {lastName ? lastName.charAt(0).toUpperCase() : ""}
                     </div>
                 </div>
             </div>
